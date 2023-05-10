6374feda7b468da0d39349a0abeffc8d686d4f9e
Anil kumar nayak <anilkumarnayak639@gmail.com>
2023-05-10 17:23:05 +0530

    functional component added

--- src/components/input.js
+++ src/components/input.js
@@ -3,48 +3,92 @@ import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import React from 'react';
 import TableData from './table';
-class InputData extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            data: "",
-            list: []
+
+const InputData = () => {
+    const [data, setData] = React.useState('')
+    const [list, setList] = React.useState([])
+
+    const submitData = () => {
+        if (data) {
+
+            setList([...list, data])
+            setData('')
         }
-        this.getData = this.getData.bind(this)
-        this.submitData = this.submitData.bind(this)
-    }
-    getData = (e) => {
-        this.setState({
-            data: e.target.value
-        })
-    }
-    submitData = (e) => {
-        e.preventDefault()
-        this.setState({
-            data: '',
-            list: [...this.state.list, this.state.data]
-        })
-
     }
 
-    render() {
-        return (
-            <div>
-                <div className='xyz'>
-                    <InputGroup className="mb-3">
-                        <Form.Control
-                            onChange={this.getData}
-                            placeholder="Write Something"
-                        />
-                        <Button variant="outline-secondary" id="button-addon2" onClick={this.submitData}>
-                            Save
-                        </Button>
-                    </InputGroup>
-                    <TableData list={this.state.list} />
-                </div>
 
+
+    return (
+        <>
+            <div className='xyz'>
+                <InputGroup className="mb-3">
+                    <Form.Control
+                        onChange={(e) => setData(e.target.value)}
+                        placeholder="Write Something"
+                        value={data}
+                    />
+                    <Button variant="outline-secondary" id="button-addon2" onClick={submitData}>
+                        Save
+                    </Button>
+                    <TableData list={list} />
+
+                </InputGroup>
             </div>
-        )
-    }
+        </>
+    )
 }
+
+
+
+
+
+
+
+
+
+
+// class InputData extends React.Component {
+//     constructor() {
+//         super()
+//         this.state = {
+//             data: "",
+//             list: []
+//         }
+//         this.getData = this.getData.bind(this)
+//         this.submitData = this.submitData.bind(this)
+//     }
+//     getData = (e) => {
+//         this.setState({
+//             data: e.target.value
+//         })
+//     }
+//     submitData = (e) => {
+//         e.preventDefault()
+//         this.setState({
+//             data: '',
+//             list: [...this.state.list, this.state.data]
+//         })
+
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <div className='xyz'>
+// <InputGroup className="mb-3">
+//     <Form.Control
+//         onChange={this.getData}
+//         placeholder="Write Something"
+//     />
+//     <Button variant="outline-secondary" id="button-addon2" onClick={this.submitData}>
+//         Save
+//     </Button>
+// </InputGroup>
+//                     <TableData list={this.state.list} />
+//                 </div>
+
+//             </div>
+//         )
+//     }
+// }
 export default InputData
--- src/components/table.js
+++ src/components/table.js
@@ -1,31 +1,66 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
 import Table from 'react-bootstrap/Table';
-class TableData extends React.Component {
-    render() {
-        return (
-            <>
-                <Table striped bordered hover>
-                    <thead>
-                        <tr>
-                            <th style={{ width: "15%" }}>Sl.No</th>
-                            <th>TASK</th>
-                        </tr>
-                    </thead>
-                    <tbody>
 
-                        {this.props.list.map((list, index) => {
-                            return (
-                                <tr>
-                                    <td key={list.index}>{index}</td>
-                                    <td>{list}</td>
-                                </tr>
-                            )
-                        })}
 
-                    </tbody>
-                </Table >
-            </>
-        )
-    }
+const TableData = ({ list }) => {
+
+    return (
+        <>
+
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th style={{ width: "15%" }}>Sl.No</th>
+                        <th>TASK</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {list.map((list, index) => {
+                        return (
+                            <tr>
+                                <td >{index + 1}</td>
+                                <td >{list}</td>
+                                <td><Button variant="success">Done</Button>{' '}</td>
+                            </tr>
+                        )
+                    })}
+
+                </tbody>
+            </Table >
+        </>
+    )
 }
+
+
+
+
+// class TableData extends React.Component {
+//     render() {
+//         return (
+//             <>
+// <Table striped bordered hover>
+//     <thead>
+//         <tr>
+//             <th style={{ width: "15%" }}>Sl.No</th>
+//             <th>TASK</th>
+//         </tr>
+//     </thead>
+//     <tbody>
+
+//         {this.props.list.map((list, index) => {
+//             return (
+//                 <tr>
+//                     <td key={list.index}>{index + 1}</td>
+//                     <td>{list}</td>
+//                 </tr>
+//             )
+//         })}
+
+//     </tbody>
+// </Table >
+//             </>
+//         )
+//     }
+// }
 export default TableData
